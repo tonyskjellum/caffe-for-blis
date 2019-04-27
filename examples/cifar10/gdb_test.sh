e29e826b5566230713d70a0faa821fa5de5f0283
carsonwoods <cwoods289@gmail.com>
2019-04-27 13:41:29 -0400

    Include gdb_test.sh

---
+++ examples/cifar10/gdb_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+
+TOOLS=./build/tools
+
+gdb --args $TOOLS/caffe train \
+  --solver=examples/cifar10/cifar10_quick_solver.prototxt $@ \
+  2>&1 | tee -a gdb_report.txt
+
+# reduce learning rate by factor of 10 after 8 epochs
+# $TOOLS/caffe train \
+    # --solver=examples/cifar10/cifar10_quick_solver_lr1.prototxt \
+    # --snapshot=examples/cifar10/cifar10_quick_iter_4000.solverstate $@
