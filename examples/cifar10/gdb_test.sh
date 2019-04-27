622f3e3421e1933d0d60a4588698778661b618f0
carsonwoods <cwoods289@gmail.com>
2019-04-27 19:02:11 -0400

    Add script to run caffe through GDB

--- examples/cifar10/gdb_test.sh
+++ examples/cifar10/gdb_test.sh
@@ -5,7 +5,7 @@ TOOLS=./build/tools
 
 gdb --args $TOOLS/caffe train \
   --solver=examples/cifar10/cifar10_quick_solver.prototxt $@ \
-  2>&1 | tee -a gdb_report.txt
+  2>&1 | tee /home/cwwoods/caffe-for-blis/logs/gdb_report.txt
 
 # reduce learning rate by factor of 10 after 8 epochs
 # $TOOLS/caffe train \
